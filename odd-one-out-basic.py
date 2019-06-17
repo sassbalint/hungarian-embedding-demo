c637f567f40e2e8453562e9a69dcd349118b4aba
Sass Bálint <sass.balint@nytud.mta.hu>
2019-06-18 00:07:51 +0200

    szószátyár (verbose) mód, kihagyott szavak áthúzva

--- odd-one-out-basic.py
+++ odd-one-out-basic.py
@@ -7,11 +7,6 @@ cors = CORS( app )
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 from gensim.models import KeyedVectors
-# key = one side of the mapping (i.e. word)
-# the other side of the mapping is the vector
-
-#import logging
-#logging.basicConfig(level=logging.INFO, format='%(levelname)-8s [%(lineno)d] %(message)s')
 
 model = KeyedVectors.load('data/glove-hu_152.gensim')
 
@@ -24,7 +19,9 @@ puzzles = [
   ['zongorázik', 'hegedül', 'fuvolázik', 'főz']
 ]
 
-ERR_MIN_THREE_TRUE = 'Please, give at least three (frequent) words separated by commas. :)'
+ERR_MIN_THREE_TRUE = 'Adjon meg legalább három (gyakori) szót, vesszővel elválasztva. :)'
+#ERR_MIN_THREE_TRUE = 'Please, give at least three (frequent) words separated by commas. :)'
+VERBOSE_COMMAND = 'verbose'
 
 @app.route( '/<string:s>', methods = ['GET','POST'] )
 @cross_origin()
@@ -34,15 +31,22 @@ def solve_( s ):
   :param s: words delimited by comma.
   :returns: the word which is the 'odd one out'.
   """
-  words = list( map( str.strip, s.split( "," ) ) )
 
   msg = [] # message
+
+  words_set = set( map( str.strip, s.split( "," ) ) )
+  VERBOSE = False
+  if VERBOSE_COMMAND in words_set:
+    VERBOSE = True
+    words_set.remove( VERBOSE_COMMAND ) 
+    msg.append( 'Szószátyár (' + VERBOSE_COMMAND + ') mód bekapcs.' )
+  words = sorted( list( words_set ) ) # magyar szerini sorrend kell! XXX
+
   ooo = "" # odd-one-out word (result)
 
   true_words = []
   oov_words = []
 
-  # XXX filter()-rel esetleg lehetne vhogy? :)
   for w in words:
     if w in model:
       true_words.append( w )
@@ -50,23 +54,31 @@ def solve_( s ):
       oov_words.append( w )
 
   if oov_words:
-    msg.append ( 'Omitted non-frequent words: ' + ', '.join(oov_words) + '.' )
+    msg.append( 'Kihagyott ritka szavak: ' + ', '.join(oov_words) )
 
   if len(true_words) < 3:
     msg.append ( ERR_MIN_THREE_TRUE )
   else:
     ooo = model.doesnt_match( true_words )
 
+  # fölső háromszög :: j > i
+  dists = ""
+  if VERBOSE:
+    for i, w1 in enumerate(true_words):
+      for j, w2 in enumerate(true_words):
+        if j > i:
+          dists += ( '[' + w1 + '|' + w2 + ': ' + "{0:5.3f}".format( model.distance( w1, w2 ) ) + ']<br/>' )
+
   res = (
-    '<html><head><title>odd one out by embedding</title></head><body><div style="width: 320px">' +
-    ', '.join( words ) + '<br/>' +
-    ( ( ', '.join( true_words ) + '<br/>' ) if words != true_words else "" ) + 
+    '<html><head><title>kakukktojás szóbeágyazással</title></head><body><div style="width: 320px">' +
+    ', '.join( true_words + list( map( lambda x: '<del>' + x + '</del>', oov_words ) ) ) + '<br/>' +
     '<i>' + '<br/>'.join( msg ) + '</i>' +
     ( ( '<br/><br/><b style="font-size: 200%">&rarr; ' + ooo + '</b>' ) if ooo else '' ) +
+    '<br/>' + dists +
     '</div></body></html>'
   )
 
-  return( res )
+  return res
 
 @app.route( '/', methods = ['GET','POST'] )
 @cross_origin()
